cace9756eb84c8e1bc7a94c8406c6261546f5bcb
Kotze <19231865@sun.ac.za>
2018-03-02 11:49:38 +0200

    working TIM2 with GPIOx

---
+++ Practical3.md
@@ -0,0 +1,17 @@
+## Practical Guide 3 
+
+
+## Callback Functions for Timer Modules
+
+
+
+
+## What timers are I using
+
+* Using the General Purpose Timers: TIM2/TIM3
+
+There main function ares on pg 479
+
+
+* T = (N*(R+1))/clock)
+
--- edesign_latest/Src/main.c
+++ edesign_latest/Src/main.c
@@ -38,8 +38,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f3xx_hal.h"
-#include <string.h>
-#include <stdlib.h>
 
 /* USER CODE BEGIN Includes */
 
@@ -115,6 +113,7 @@ int main(void)
 
   /* USER CODE BEGIN SysInit */
   /* USER CODE END SysInit */
+
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
@@ -130,22 +129,17 @@ int main(void)
 
 
   /* USER CODE END WHILE */
-//	  HAL_UART_Receive_IT(&huart1, rx_buffer, 1);
-		//HAL_UART_Receive_IT(&huart1, &rx_buffer, 1);
+
   /* USER CODE BEGIN 3 */
 	  if(rx_flag == 1){
 		  HAL_UART_Receive_IT(&huart1, (uint8_t*)&rx_buffer, 1);
 		  rx_flag = 0;
-		  uart_command[uart_counter] = rx_buffer;
-		  uart_counter += 1;
 
 
 
 			  if(uart_command[0] == '$' && uart_command[uart_counter-1] == '\n' && uart_command[uart_counter - 2] == '\r' && uart_counter > 2){
-//					 return_val[0] = uart_command[0];
-//					 return_val[1] = uart_command[1];
-//					 return_val[2] = uart_command[uart_counter -2];
-//					 return_val[3] = uart_command[uart_counter -1];
+				  uart_command[uart_counter] = rx_buffer;
+				  uart_counter += 1;
 
 					 switch(uart_command[1]){
 
@@ -263,6 +257,14 @@ int main(void)
 	  }
 
   }
+
+
+
+
+
+
+
+
   /* USER CODE END 3 */
 
 }
@@ -343,13 +345,63 @@ static void MX_USART1_UART_Init(void)
 
 }
 
-/** Pinout Configuration
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
 */
 static void MX_GPIO_Init(void)
 {
 
+  GPIO_InitTypeDef GPIO_InitStruct;
+
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8 
+                          |GPIO_PIN_9, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PC0 PC1 PC7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA0 PA1 PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA5 PA6 PA7 PA8 
+                           PA9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8 
+                          |GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB10 PB8 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB3 PB4 PB5 PB6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
