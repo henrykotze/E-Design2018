69b46f3e05f4eb0b704a4235b27ebf6aff0b89f1
labuser <labuser>
2018-05-04 12:41:54 +0200

    heating scheduling planning tbc

--- edesign19231865/Src/heating_schedule.c
+++ edesign19231865/Src/heating_schedule.c
@@ -13,11 +13,37 @@ void heating_scheduling(){
 	//HAL_RTC_GetTime(&hrtc,time,RTC_FORMAT_BIN);
 
 	// checking heating schedule 1
-//	if(time->Hours > heating_schedule1)
+	if(time->Hours > heating_schedule[0].Hours){
+		if(time->Minutes > heating_schedule[0].Minutes){
+			if(time->Seconds > heating_schedule[0].Seconds){
+				// switch on
+			}
+			else{}
+		}
+	}
+	// heating schedule 2
+	if(time->Hours > heating_schedule[1].Hours){
+		if(time->Minutes > heating_schedule[1].Minutes){
+			if(time->Seconds > heating_schedule[1].Seconds){
+				// switch on
+			}
+			else{}
+		}
+	}
+
+	// heating schedule 3
+	if(time->Hours > heating_schedule[2].Hours){
+		if(time->Minutes > heating_schedule[2].Minutes){
+			if(time->Seconds > heating_schedule[3].Seconds){
+				// switch on
+			}
+			else{}
+		}
+	}
 
 
 
 
 
 
-}
+	}
