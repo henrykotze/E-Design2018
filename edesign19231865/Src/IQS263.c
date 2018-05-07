39cb06389112cda2f22f4f60a25105fe8e727d0f
Henry Kotze <19231865@sun.ac.za>
2018-05-07 23:01:35 +0200

    continued with i2c

--- edesign19231865/Src/IQS263.c
+++ edesign19231865/Src/IQS263.c
@@ -10,7 +10,7 @@ extern TIM_HandleTypeDef htim3;
 extern UART_HandleTypeDef huart1;
 extern ADC_HandleTypeDef hadc2;
 extern RTC_HandleTypeDef hrtc;
-
+extern I2C_HandleTypeDef hi2c1;
 
 
 
@@ -19,10 +19,11 @@ static int TOUCH_THRESHOLD = 0x20;
 static int TOUCH_THRESHOLD_CH1 = 0x20; //0x20
 static int TOUCH_THRESHOLD_CH2 = 0x20; //0x20
 static int TOUCH_THRESHOLD_CH3 = 0x20; //0x20
-static int MOVEMENT_THRESHOLD = 0x03;
-static int RESEED_BLOCK = 0x00;
+
+//static int MOVEMENT_THRESHOLD = 0x03;
+//static int RESEED_BLOCK = 0x00;
 static int HALT_TIME = 0x00;
-static int I2C_TIMEOUT = 0x04;
+//static int I2C_TIMEOUT = 0x04;
 
 
 
@@ -31,7 +32,6 @@ uint8_t CH2_Event=0;
 uint8_t CH3_Event=0;
 uint8_t IQS263_Threshold[8];
 
-extern I2C_HandleTypeDef  hi2cxt;
 
 
 /**
@@ -46,13 +46,13 @@ void Init_IQS263( )
 
 	/* GPIO Ports Clock Enable */
 
-	__HAL_RCC_GPIOA_CLK_ENABLE();
+	//__HAL_RCC_GPIOA_CLK_ENABLE();
 
 	/*Configure GPIO pin : SD_CS_Pin */
 	GPIO_InitStruct.Pin = GPIO_PIN_1;
 	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
 	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   	uint8_t data_buffer[8]={0};
@@ -60,19 +60,19 @@ void Init_IQS263( )
 
 
 	/*....................READING SYSTEM FLAGS..........................*/
-	 while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1);
+	 while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1){}
 
-		HAL_I2C_Mem_Read( &hi2cxt, IQS263_ADD, (uint8_t)SYS_FLAGS, I2C_MEMADD_SIZE_8BIT,  data_buffer, 2,50);
-		while (HAL_I2C_GetState(&hi2cxt) != HAL_I2C_STATE_READY)
+		HAL_I2C_Mem_Read( &hi2c1, IQS263_ADD, (uint8_t)SYS_FLAGS, I2C_MEMADD_SIZE_8BIT,  data_buffer, 2,50);
+		while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
 		{
 		}
 
 
 	/*....................READING DEVICE INFO..........................*/
-	 while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1);
+	 while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1){}
 
-		HAL_I2C_Mem_Read(&hi2cxt, IQS263_ADD,( uint8_t ) DEVICE_INFO, I2C_MEMADD_SIZE_8BIT,  &data_buffer[0],2,	70);
-		while (HAL_I2C_GetState(&hi2cxt) != HAL_I2C_STATE_READY)
+		HAL_I2C_Mem_Read(&hi2c1, IQS263_ADD,( uint8_t ) DEVICE_INFO, I2C_MEMADD_SIZE_8BIT,  &data_buffer[0],2,	70);
+		while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
 		{
 		}
 
@@ -80,8 +80,8 @@ void Init_IQS263( )
 
 		while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1);
 
-		HAL_I2C_Mem_Read( &hi2cxt, IQS263_ADD, ACTIVE_CHANNELS, I2C_MEMADD_SIZE_8BIT,  data_buffer, 1,60);
-		while (HAL_I2C_GetState(&hi2cxt) != HAL_I2C_STATE_READY)
+		HAL_I2C_Mem_Read( &hi2c1, IQS263_ADD, ACTIVE_CHANNELS, I2C_MEMADD_SIZE_8BIT,  data_buffer, 1,60);
+		while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
 		{
 		}
 
@@ -89,8 +89,8 @@ void Init_IQS263( )
 
 		while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1);
 
-		HAL_I2C_Mem_Read( &hi2cxt, IQS263_ADD, THRESHOLDS, I2C_MEMADD_SIZE_8BIT,  IQS263_Threshold, 8,60);
-		while (HAL_I2C_GetState(&hi2cxt) != HAL_I2C_STATE_READY)
+		HAL_I2C_Mem_Read( &hi2c1, IQS263_ADD, THRESHOLDS, I2C_MEMADD_SIZE_8BIT,  IQS263_Threshold, 8,60);
+		while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
 		{
 		}
 
@@ -107,44 +107,44 @@ void Init_IQS263( )
 
 		while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1);
 
-		HAL_I2C_Mem_Write( &hi2cxt, IQS263_ADD, THRESHOLDS, I2C_MEMADD_SIZE_8BIT,  IQS263_Threshold, 8,60);
-	  while (HAL_I2C_GetState(&hi2cxt) != HAL_I2C_STATE_READY)
+		HAL_I2C_Mem_Write( &hi2c1, IQS263_ADD, THRESHOLDS, I2C_MEMADD_SIZE_8BIT,  IQS263_Threshold, 8,60);
+	  while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
 		{
 		}
-		while (HAL_I2C_IsDeviceReady(&hi2cxt, IQS263_ADD, 10, 300) == HAL_TIMEOUT);
+		while (HAL_I2C_IsDeviceReady(&hi2c1, IQS263_ADD, 10, 300) == HAL_TIMEOUT);
 
 		/*....................READING THRESHOLDS..........................*/
 
 		while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1);
 
-		HAL_I2C_Mem_Read( &hi2cxt, IQS263_ADD, THRESHOLDS, I2C_MEMADD_SIZE_8BIT,  IQS263_Threshold, 8,60);
-	 	while (HAL_I2C_GetState(&hi2cxt) != HAL_I2C_STATE_READY)
+		HAL_I2C_Mem_Read( &hi2c1, IQS263_ADD, THRESHOLDS, I2C_MEMADD_SIZE_8BIT,  IQS263_Threshold, 8,60);
+	 	while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
 		{
 		}
-		while (HAL_I2C_IsDeviceReady(&hi2cxt, IQS263_ADD, 10, 300) == HAL_TIMEOUT);
+		while (HAL_I2C_IsDeviceReady(&hi2c1, IQS263_ADD, 10, 300) == HAL_TIMEOUT)
 
 		/*....................READING PROXI SETTING..........................*/
 
 		 while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1);
 
-	   HAL_I2C_Mem_Read( &hi2cxt, IQS263_ADD, PROX_SETTINGS, I2C_MEMADD_SIZE_8BIT,  data_buffer, 5,60);
-	   while (HAL_I2C_GetState(&hi2cxt) != HAL_I2C_STATE_READY)
+	   HAL_I2C_Mem_Read( &hi2c1, IQS263_ADD, PROX_SETTINGS, I2C_MEMADD_SIZE_8BIT,  data_buffer, 5,60);
+	   while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
 		 {
 		 }
-		 while (HAL_I2C_IsDeviceReady(&hi2cxt, IQS263_ADD, 10, 300) == HAL_TIMEOUT);
+		 while (HAL_I2C_IsDeviceReady(&hi2c1, IQS263_ADD, 10, 300) == HAL_TIMEOUT);
 
 		/*....................WRITING PROXI SETTING..........................*/
 
-	  while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1);
+	  while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1)
 
 		  data_buffer[0]=0;
 			data_buffer[2]=8;
 
-		HAL_I2C_Mem_Write( &hi2cxt, IQS263_ADD, PROX_SETTINGS, I2C_MEMADD_SIZE_8BIT,  data_buffer, 5,	60);
-	 while (HAL_I2C_GetState(&hi2cxt) != HAL_I2C_STATE_READY)
+		HAL_I2C_Mem_Write( &hi2c1, IQS263_ADD, PROX_SETTINGS, I2C_MEMADD_SIZE_8BIT,  data_buffer, 5,	60);
+	 while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
 		{
 		}
-		while (HAL_I2C_IsDeviceReady(&hi2cxt, IQS263_ADD, 10, 300) == HAL_TIMEOUT);
+		while (HAL_I2C_IsDeviceReady(&hi2c1, IQS263_ADD, 10, 300) == HAL_TIMEOUT);
 
 
 }
@@ -162,9 +162,9 @@ void IQS263_READ_TOUCH_Events(){
 	uint8_t data[2]={0};
 /*....................READING TOUCH EVENTS..........................*/
 
-	while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1);
+	while(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_1)==1)
 
-		HAL_I2C_Mem_Read(&hi2cxt, IQS263_ADD, TOUCH_BYTES, I2C_MEMADD_SIZE_8BIT,data,1,70);
+		HAL_I2C_Mem_Read(&hi2c1, IQS263_ADD, TOUCH_BYTES, I2C_MEMADD_SIZE_8BIT,data,1,70);
 
 
 	  CH1_Event=data[0]&0x02;
